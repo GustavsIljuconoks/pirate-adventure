cc25dcae9b01c4565e0be14eb96af0d5da1c1891
Gustavs Iļjučonoks <gustavs.iljuconoks@cube.lv>
2023-11-16 13:02:24 +0200

    Folder changes

---
+++ src/front-end/src/App.tsx
@@ -0,0 +1,18 @@
+import LoadingOrError from 'components/LoadingOrError'
+import type { ReactElement } from 'react'
+import { Suspense, lazy } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+const Main = lazy(async () => import('pages/Main'))
+
+export default function App(): ReactElement {
+    return (
+        <BrowserRouter>
+            <Suspense fallback={<LoadingOrError />}>
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                </Routes>
+            </Suspense>
+        </BrowserRouter>
+    )
+}
---
+++ src/front-end/src/pages/Main.tsx
@@ -0,0 +1,9 @@
+import type { ReactElement } from 'react'
+
+export default function GalleryPage(): ReactElement {
+    return (
+        <div className="m-2 grid min-h-screen">
+            <p>Hey this is BattleShip game `Pirate&apos;s adventure`</p>
+        </div>
+    )
+}
