1ff47918381e04f2a2fdb3a34ea21940f9f93cc6
Gustavs Iļjučonoks <gustavs.iljuconoks@cube.lv>
2024-01-14 22:10:45 +0200

    Created field initialization api layer

--- src/back-end/BattleshipPirateAdventure.Core/Models/Location.cs
+++ src/back-end/BattleshipPirateAdventure.Core/Models/Location.cs
@@ -2,7 +2,7 @@ namespace BattleshipPirateAdventure.Core.Models;
 
 public class Location(string cellId, int column, int row) : IEquatable<Location>
 {
-    public int Row { get; } = row;
+    public int Row { get; set; } = row;
     public int Column { get; set; } = column;
     internal string CellId { get; } = cellId;
 
