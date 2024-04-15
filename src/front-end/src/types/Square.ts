aca74c62303412041c43cda24f9ded0855b84d59
Gustavs Iļjučonoks <gustavs.iljuconoks@cube.lv>
2024-04-15 10:39:42 +0300

    Remove unnecessary

--- src/front-end/src/types/Square.ts
+++ src/front-end/src/types/Square.ts
@@ -8,19 +8,6 @@ export type CellType = {
   state: CellState
 }
 
-export const Columns = {
-  A: 0,
-  B: 1,
-  C: 2,
-  D: 3,
-  E: 4,
-  F: 5,
-  G: 6,
-  H: 7,
-  I: 8,
-  J: 9
-}
-
 export const Rows = Array.from({ length: 10 }, (_, i) => i)
 
 export type FieldType = CellType[]
