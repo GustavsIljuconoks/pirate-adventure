2f32849685109c4cc74c29258bcf85ececf60647
Gustavs Iļjučonoks <gustavs.iljuconoks@cube.lv>
2024-04-22 11:55:15 +0300

    dev: remove PWA support

--- src/front-end/src/constants.ts
+++ src/front-end/src/constants.ts
@@ -1,5 +1,5 @@
-export const SERVER_URL: string = 'https://localhost:7035'
-export const APP_URL: string = 'http://localhost:5173/'
+export const SERVER_URL: string = import.meta.env.VITE_SERVER_URL
+export const APP_URL: string = import.meta.env.VITE_APP_URL
 
 export const COLUMNS_COUNT: number = 10
 export const ROWS_COUNT: number = 10
